6742e36c79bc8714572d2fd913e8406511c7c07e
Destan Tezel <destantezel@googlemail.com>
2023-12-18 22:42:17 +0000

    Create 'Profile' model and connect signal to User

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_lcovgw'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+# Parameters required by the post_save.connect method
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+# Django's post_save singal
+post_save.connect(create_profile, sender=User)
