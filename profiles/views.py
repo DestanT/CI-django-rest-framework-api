2b44bf21fd48ae9e146906695a2904b191a4bbf0
Destan Tezel <destantezel@googlemail.com>
2024-01-05 17:32:50 +0000

    Use .annotate and Count() to add to the queryset

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -10,7 +11,11 @@ class ProfileList(generics.ListAPIView):
     No create view as profile creation is handled by django signals.
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
